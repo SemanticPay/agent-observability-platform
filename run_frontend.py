3b3738fc2774f4bca4456be213243fc0f9048d47
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-11-11 18:53:03 +0400

    chore: Remove old frontend
    Signed-off-by: Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>

--- run_frontend.py
+++
@@ -1,8 +0,0 @@
-"""Run the frontend Flask application."""
-from agent.front.app import app
-
-if __name__ == '__main__':
-    print("Starting Driver's License Renewal Agent Frontend...")
-    print("Open http://localhost:5000 in your browser")
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
