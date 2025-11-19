44d3deffcab834fa2d420988f9895d62de9cefb6
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-11-19 20:03:33 +0400

    feat: Add new frontend
    Signed-off-by: Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>

---
+++ agent/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import ChatPage from "./components/ChatPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ agent/frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { ChatProvider } from "./context/ChatContext"
+
+createRoot(document.getElementById("root")!).render(
+  <ChatProvider>
+    <App />
+  </ChatProvider>
+)
