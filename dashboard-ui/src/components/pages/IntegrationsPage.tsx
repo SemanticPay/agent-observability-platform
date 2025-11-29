7d281dc6acffc4e3f2fbe60590d533be3aa1f1db
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-11-29 22:52:29 +0400

    Revert "feat: Add dashboard UI"
    This reverts commit 63f930a2779ef3ae7f1fd8d1d2788ab7e4946fb6.

--- dashboard-ui/src/components/pages/IntegrationsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { PlatformIntegrations } from "../PlatformIntegrations";
-
-export function IntegrationsPage() {
-  return (
-    <div className="max-w-[1800px] mx-auto px-8 py-8 space-y-8">
-      <PlatformIntegrations />
-    </div>
-  );
-}
