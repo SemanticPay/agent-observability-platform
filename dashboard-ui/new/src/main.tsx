ddc3f77f97b142e2dd4f3d94782eb44069e33e4f
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-12-10 11:58:13 +0400

    feat: Add chat with dashboard for UI v2 with copilotkit
    Signed-off-by: Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>

--- dashboard-ui/new/src/main.tsx
+++ dashboard-ui/new/src/main.tsx
@@ -1,7 +1,12 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
+  import RootLayout from "./layout.tsx";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <RootLayout>
+      <App />
+    </RootLayout>
+  );
   
